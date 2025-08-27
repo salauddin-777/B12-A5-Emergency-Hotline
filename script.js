5a7a7565c740a4c31da8ff28fa880182ce3fa97c
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-08-27 17:20:37 +0600

    Heart functionality with Javascript

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // console.log('Ami achi vai');
 
-//Like Area
+//Like Button Area
 
-document.getElementsByClassName('btn-like').addEventListener('click', function(){
-    
-    console.log('Hello kaka');
-    }
-)
+const likeBtn = document.querySelectorAll('.btn-like');
+for( let btn of likeBtn){
+    btn.addEventListener('click', function(){
+        const likeArea = document.getElementById('like-area');
+         let newLike =  parseInt(likeArea.innerText);
+         newLike++;
+         likeArea.innerText = newLike;
+    })
+}
+
+// Call Button Area
+
+const callBtn = document.querySelectorAll('.btn-call');
+for( let btn of callBtn){
+    btn.addEventListener('click', function(){
+        alert(`Calling `);
+    })
+}
